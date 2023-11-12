5a2c959cb5ae0409d29a6cab563ed655dccd1001
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2023-11-12 21:48:36 +0200

    Initial Commit

---
+++ source/common/shader/shader.hpp
@@ -0,0 +1,85 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+#include <string>
+
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace our {
+
+    class ShaderProgram {
+
+    private:
+        //Shader Program Handle (OpenGL object name)
+        GLuint program;
+
+    public:
+        // Tarek
+        ShaderProgram(){
+            //TODO: (Req 1) Create A shader program
+            GLuint program = glCreateProgram();
+        }
+        // Tarek
+        ~ShaderProgram(){
+            //TODO: (Req 1) Delete a shader program
+        }
+
+        // Tarek
+        bool attach(const std::string &filename, GLenum type) const;
+
+        // Tarek
+        bool link() const;
+
+        void use() { 
+            glUseProgram(program);
+        }
+
+        // Zaki
+        GLuint getUniformLocation(const std::string &name) {
+            //TODO: (Req 1) Return the location of the uniform with the given name
+        }
+
+        // Osama
+        void set(const std::string &uniform, GLfloat value) {
+            //TODO: (Req 1) Send the given float value to the given uniform
+        }
+
+        // Osama
+        void set(const std::string &uniform, GLuint value) {
+            //TODO: (Req 1) Send the given unsigned integer value to the given uniform
+        }
+
+        // Osama
+        void set(const std::string &uniform, GLint value) {
+            //TODO: (Req 1) Send the given integer value to the given uniform
+        }
+
+        // Waleed
+        void set(const std::string &uniform, glm::vec2 value) {
+            //TODO: (Req 1) Send the given 2D vector value to the given uniform
+        }
+
+        // Waleed
+        void set(const std::string &uniform, glm::vec3 value) {
+            //TODO: (Req 1) Send the given 3D vector value to the given uniform
+        }
+
+        // Waleed
+        void set(const std::string &uniform, glm::vec4 value) {
+            //TODO: (Req 1) Send the given 4D vector value to the given uniform
+        }
+
+        // Zaki
+        void set(const std::string &uniform, glm::mat4 matrix) {
+            //TODO: (Req 1) Send the given matrix 4x4 value to the given uniform
+        }
+
+        //TODO: (Req 1) Delete the copy constructor and assignment operator.
+        //Question: Why do we delete the copy constructor and assignment operator?
+    };
+
+}
+
+#endif
