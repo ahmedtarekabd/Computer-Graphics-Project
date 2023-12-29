f8406169a044606f805ddc5373f022cc7e46b41a
YoussefZaki119 <youssefzaki2002@gmail.com@DESKTOP-LHVS2PK>
2023-12-29 21:11:18 +0200

    collision

--- source/common/systems/movement.hpp
+++ source/common/systems/movement.hpp
@@ -12,26 +12,49 @@ namespace our
 {
 
     // The movement system is responsible for moving every entity which contains a MovementComponent.
-    // This system is added as a simple example for how use the ECS framework to implement logic. 
+    // This system is added as a simple example for how use the ECS framework to implement logic.
     // For more information, see "common/components/movement.hpp"
-    class MovementSystem {
+    class MovementSystem
+    {
     public:
-
-        // This should be called every frame to update all entities containing a MovementComponent. 
-        void update(World* world, float deltaTime) {
+        // This should be called every frame to update all entities containing a MovementComponent.
+        void update(World *world, float deltaTime)
+        {
             // For each entity in the world
-            for(auto entity : world->getEntities()){
+            Entity *player;
+            for (auto entity : world->getEntities())
+            {
+
+                if (entity->name == "player")
+                {
+                    player = entity;
+                }
+
                 // Get the movement component if it exists
-                MovementComponent* movement = entity->getComponent<MovementComponent>();
+            }
+
+            for (auto entity : world->getEntities())
+            {
+                MovementComponent *movement = entity->getComponent<MovementComponent>();
                 // If the movement component exists
-                if(movement){
+                if (movement)
+                {
+
+                    if (entity->name == "monster")
+                    {
+                        auto direction = player->localTransform.position - entity->localTransform.position;
+                        direction = normalize(direction);
+                        direction.y =0;
+                        movement->linearVelocity = direction * 1.3f;
+
+                        auto directionangle= atan2(direction.x,direction.z);
+                    }
                     // Change the position and rotation based on the linear & angular velocity and delta time.
                     entity->localTransform.position += deltaTime * movement->linearVelocity;
                     entity->localTransform.rotation += deltaTime * movement->angularVelocity;
                 }
             }
         }
-
     };
 
 }
